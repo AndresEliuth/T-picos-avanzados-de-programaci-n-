8c1a1ed793a574a049a2db63224491022eecc0fd
AndresEliuth <andresamadorcrus@gmail.com>
2026-01-29 20:45:29 -0600

    cree la estructura de mis carpetas

---
+++ Beta/IntroduccionAPHP.php
@@ -0,0 +1,10 @@
+<?php
+echo "hola mundo \n";// se debe poner \n para los saltos de linea
+
+//variables
+$Primera="Cadena de texto";//para declarar una variable de tipo string se utiliza $ y ""
+echo $Primera . "\n";
+$Primera=28;// para declara un int se necesita solomponer el numero sin ""
+echo $Primera;
+
+?>
