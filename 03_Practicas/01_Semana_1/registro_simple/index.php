791325c6093cf9ae228879bc1660caa548dd6865
AndresEliuth <andresamadorcrus@gmail.com>
2026-02-04 21:36:24 -0600

    Realice mi formulario donde solicito informacion de min usuario

---
+++ 03_Practicas/01_Semana_1/registro_simple/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulario de ejemplo</title>
+</head>
+<body>
+
+<h1>Formulario de ejemplo</h1>
+
+<form>
+    <label>Nombre completo:</label>
+    <input type="text">
+
+    <br><br>
+
+    <label>Correo:</label>
+    <input type="text">
+
+    <br><br>
+    <label>Numero:</label>
+    <input type="number">
+
+    <br><br>
+
+    <label>Contraseña:</label>
+    <input type="text">
+
+    <br><br>
+    <br><br>
+
+    
+
+    <!-- Botones -->
+    <button>Registrar</button>
+    <button>Limpiar</button>
+</form>
+
+</body>
+</html>
